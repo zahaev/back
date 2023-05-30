58a10bcca898d76ba5fc85077b3b191db9a493ed
zahaev <113417779+zahaev@users.noreply.github.com>
2023-05-30 19:21:49 +0300

    Update admin.php

--- 6/admin.php
+++ 6/admin.php
@@ -3,7 +3,7 @@
 if($_SERVER['REQUEST_METHOD']=='GET'){
   $user = 'u52943';
   $pass = '2352838';
-  $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $db = new PDO('mysql:host=localhost;dbname=u52943', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
   $pass_hash=array();
   try{
     $get=$db->prepare("select pass from admin where user=?");
